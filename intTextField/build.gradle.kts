5fb9867915f037013e352b93dc0e3f41f6aa124a
Rikuto13ten <rikuto1367@gmail.com>
2024-03-10 23:09:32 +0900

    add: jitpack

--- intTextField/build.gradle.kts
+++ intTextField/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("com.github.Rikuto13ten:IntTextField:1.0.0")
 }
 
 afterEvaluate {
